8cc4098b2c064be0cd37234e50a6886bb2ddfafb
kudratovazizbek2008 <kudratovazizbek2008@gmail.com>
2025-06-04 00:38:40 -0700

    creat uyga

--- main.js
+++ main.js
@@ -1,50 +1,39 @@
-let list = document.querySelector("#list")
-let innerList = document.querySelector("#innerList")
+let list = document.querySelector("#list");
+
 let fetchCountries = async (url) => {
-    let res = await fetch(url)
-    let json = await res.json()
+    let res = await fetch(url);
+    let json = await res.json();
 
-    WriteCountries(json)
-}
+    WriteCountries(json);
+};
 
-fetchCountries("https://restcountries.com/v3.1/all")
+fetchCountries("https://restcountries.com/v3.1/all");
 
 function WriteCountries(data) {
     data.forEach((country) => {
+        let li = document.createElement("li");
 
-        let li = document.createElement("li")
-        li.innerHTML = `<div class="bg-gray-300 rounded-md ">
-<h3 class="text-center font-bold text-[20px]">
-${country.name.common}
-</h3>
-<img class="w-[200px] h-[100px] mx-auto" src="${country.flags.png}" alt="${country.flags.alt}">
-</div>`
+        li.innerHTML = `
+      <div class="bg-gray-300 rounded-md p-2 cursor-pointer">
+        <h3 class="text-center font-bold text-[20px]">${country.name.common}</h3>
+        <img class="w-[200px] h-[100px] mx-auto" src="${country.flags.png}" alt="${country.flags.alt}">
+        
+        <div class="dropdown hidden mt-2 bg-white rounded-md p-2 shadow">
+          <p><strong>Name:</strong> ${country.name.common}</p>
+          <p><strong>Region:</strong> ${country.region}</p>
+          <p><strong>Area:</strong> ${country.area}</p>
+          <img class="w-[200px] h-[100px] mx-auto mt-2" src="${country.flags.png}" alt="${country.flags.alt}">
+        </div>
+      </div>
+    `;
 
+        let card = li.querySelector("div");
+        let dropdown = li.querySelector(".dropdown");
 
+        card.addEventListener("click", () => {
+            dropdown.classList.toggle("hidden");
+        });
 
-        li.addEventListener("click", () => {
-            InnerFunction(country)
-        })
-        list.appendChild(li)
-    })
-}
-
-
-function InnerFunction(country) {
-    list.style.display = "none"
-
-    console.log(country)
-    innerList.innerHTML = `
-<div class="bg-gray-300 rounded-md ">
-<p>Name: ${country.name.common}</p>
-<p>Region: ${country.region}</p>
-<p>Area: ${country.area}</p>
-
-<img class="w-[200px] h-[100px] mx-auto" src="${country.flags.png}" alt="${country.flags.alt}">
-
-
-</div>
-
-`
-    let card = document.querySelector("div")
+        list.appendChild(li);
+    });
 }
